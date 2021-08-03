e8fcb27936d4636a272a9a0c48d5f41107995c3e
kelevra-pro <damtat8@gmail.com>
2021-08-04 00:20:21 +0300

    resolve js functions map

---
+++ 09-js-functions/09-map.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    name: 'Tirion',
+    children: [
+      { name: 'Mira', birthday: '1983-03-23' },
+    ],
+  },
+  { name: 'Bronn', children: [] },
+  {
+    name: 'Sam',
+    children: [
+      { name: 'Aria', birthday: '2012-11-03' },
+      { name: 'Keit', birthday: '1933-05-14' },
+    ],
+  },
+  {
+    name: 'Rob',
+    children: [
+      { name: 'Tisha', birthday: '2012-11-03' },
+    ],
+  },
+];
+
+const getChildren = (arr) => {
+  const childrenOfUsers = arr.map(({ children }) => children);
+
+  return childrenOfUsers.flat();
+};
+
+// export default getChildren;
+
+getChildren(users);
+// [
+//   { name: 'Mira', birthday: '1983-03-23' },
+//   { name: 'Aria', birthday: '2012-11-03' },
+//   { name: 'Keit', birthday: '1933-05-14' },
+//   { name: 'Tisha', birthday: '2012-11-03' },
+// ];
