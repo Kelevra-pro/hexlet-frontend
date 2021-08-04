40c9e11192b9984fb7f924ad8c89d968ab23c10d
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2021-08-05 00:13:35 +0300

    resolve js functions filter (#18)

---
+++ 09-js-functions/10-filter.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    name: 'Tirion',
+    friends: [
+      { name: 'Mira', gender: 'female' },
+      { name: 'Ramsey', gender: 'male' },
+    ],
+  },
+  { name: 'Bronn', friends: [] },
+  {
+    name: 'Sam',
+    friends: [
+      { name: 'Aria', gender: 'female' },
+      { name: 'Keit', gender: 'female' },
+    ],
+  },
+  {
+    name: 'Rob',
+    friends: [
+      { name: 'Taywin', gender: 'male' },
+    ],
+  },
+];
+
+const getGirlFriends = (users) => {
+  const friendsOfUsers = users.map(({ friends }) => friends);
+
+  return friendsOfUsers.flat().filter(({ gender }) => gender === 'female');
+};
+
+export default getGirlFriends;
+
+getGirlFriends(users);
+// [
+//   { name: 'Mira', gender: 'female' },
+//   { name: 'Aria', gender: 'female' },
+//   { name: 'Keit', gender: 'female' },
+// ];
