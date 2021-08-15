976fda28ca793b767fd368fd1b4c48b07d3f864b
kelevra-pro <damtat8@gmail.com>
2021-08-15 21:04:33 +0300

    resolve js functions signals

---
+++ 09-js-functions/11-signals.js
@@ -0,0 +1,41 @@
+import get from 'lodash/get';
+
+const freeEmailDomains = [
+  'gmail.com',
+  'yandex.ru',
+  'hotmail.com',
+  'yahoo.com',
+];
+const emails = [
+  'info@gmail.com',
+  'info@yandex.ru',
+  'info@hotmail.com',
+  'mk@host.com',
+  'support@hexlet.io',
+  'key@yandex.ru',
+  'sergey@gmail.com',
+  'vovan@gmail.com',
+  'vovan@hotmail.com',
+];
+
+const getFreeDomainsCount = (emails) => emails
+.map((email) => {
+  const [, domain] = email.split('@');
+
+  return domain;
+})
+.filter((domain) => freeEmailDomains.includes(domain))
+.reduce((acc, domain) => {
+  const count = get(acc, domain, 0) + 1;
+
+  return { ...acc, [domain]: count };
+}, {});
+
+export default getFreeDomainsCount;
+
+getFreeDomainsCount(emails);
+// {
+//   'gmail.com': 3,
+//   'yandex.ru': 2,
+//   'hotmail.com': 2,
+// };
