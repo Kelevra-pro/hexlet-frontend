9d75ea4e6f2a421a28c9ac0bae734d0794a68dd3
kelevra-pro <damtat8@gmail.com>
2021-08-25 23:17:44 +0300

    fix js testing goal

--- 09-js-testing/02-goal.js
+++ 09-js-testing/02-goal.js
@@ -1,17 +1,13 @@
-// Идея функции взята из lodash
-// https://lodash.com/docs#get
-get({ hello: 'world' }, 'hello'); // world
-get({ hello: 'world' }, 'hello', 'kitty'); // 'world'
-get({}, 'hello', 'kitty'); // 'kitty'
+import _ from 'lodash';
 
-if (get({ hello: 'world' }, 'hello') !== 'world') {
+if (_.get({ hello: 'world' }, 'hello') !== 'world') {
   throw new Error('Error');
 }
 
-if (get({}, 'hello', 'kitty') !== 'kitty') {
+if (_.get({}, 'hello', 'kitty') !== 'kitty') {
   throw new Error('Error');
 }
 
-if (get({ hello: 'world' }, 'hello', 'kitty') !== 'world') {
+if (_.get({ hello: 'world' }, 'hello', 'kitty') !== 'world') {
   throw new Error('Error');
 }
