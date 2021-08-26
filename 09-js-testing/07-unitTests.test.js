81a4e82a1761cf638a45da5fb02052935bb5e17f
kelevra-pro <damtat8@gmail.com>
2021-08-26 18:32:50 +0300

    resolve js testing unit-tests

---
+++ 09-js-testing/07-unitTests.test.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import makeValidator from './07-unitTests';
+
+test('makeValidator', () => {
+  const validator = makeValidator();
+
+  expect(validator.isValid('some value')).toBe(true);
+  expect(validator.isValid()).toBe(true);
+
+  validator.addCheck(_.isNumber);
+  expect(validator.isValid('some value')).toBe(false);
+
+  validator.addCheck((v) => v > 10);
+  expect(validator.isValid(11)).toBe(true);
+
+  validator.addCheck((v) => v % 2 === 0);
+  expect(validator.isValid(11)).toBe(false);
+  expect(validator.isValid(12)).toBe(true);
+  expect(validator.isValid(8)).toBe(false);
+});
