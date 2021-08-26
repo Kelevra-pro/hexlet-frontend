94691eb5524e58c1192354e89378be748af02135
kelevra-pro <damtat8@gmail.com>
2021-08-26 03:30:35 +0300

    fix js testing matchers

--- 09-js-testing/06-matchers.test.js
+++ 09-js-testing/06-matchers.test.js
@@ -1,7 +1,7 @@
 import gt from './06-matchers';
 
 test('gt', () => {
-  expect(gt(3, 1)).toBeTruthy();
-  expect(gt(3, 3)).toBeFalsy();
-  expect(gt(1, 3)).toBeFalsy();
+  expect(gt(3, 1)).toBe(true);
+  expect(gt(3, 3)).toBe(false);
+  expect(gt(1, 3)).toBe(false);
 });
