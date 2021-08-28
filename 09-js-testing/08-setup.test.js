5a29806f153e97e5cc26317a44a887d202c5769f
kelevra-pro <damtat8@gmail.com>
2021-08-29 01:00:16 +0300

    fix 08-setup.test.js

--- 09-js-testing/08-setup.test.js
+++ 09-js-testing/08-setup.test.js
@@ -1,19 +1,6 @@
 import _ from 'lodash';
 import set from './08-setup';
 
-// const object = { a: [{ b: { c: 3 } }] };
-//
-// beforeAll(() => {
-//   set(object, 'a[0].b.c', 4);
-//   set(object, ['x', '0', 'y', 'z'], 5);
-// });
-//
-// test('set1', () => {
-//   expect(object.a[0].b.c).toBe(4);
-//   expect(object.x[0].y.z).toBe(5);
-//   expect(object.key).not.toBe('value');
-// });
-
 let data;
 let dataCopy;
 
