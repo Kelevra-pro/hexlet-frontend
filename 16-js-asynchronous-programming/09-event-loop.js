f3582f2ae1946e1b332743b652dc5ccf77f14583
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2022-01-19 00:11:32 +0300

    resolve js-asynchronous-programming event-loop (#67)

---
+++ 16-js-asynchronous-programming/09-event-loop.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import async from 'async';
+
+const { waterfall } = async;
+
+export const unionFiles = (inputPath1, inputPath2, outputPath, cb) => {
+  waterfall([
+    (callback) => fs.readFile(inputPath1, callback),
+    (data1, callback) => fs.readFile(inputPath2, (err, data2) => callback(err, data1, data2)),
+    (data1, data2, callback) => fs.writeFile(outputPath, `${data1}${data2}`, callback),
+  ], cb);
+};
