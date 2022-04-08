f5d39b3ce06e8ca1663407be7dff20e5f5e97e78
kelevra-pro <damtat8@gmail.com>
2022-04-08 15:32:54 +0300

    resolve js-dom tree-manipulating

---
+++ 20-js-dom/09-tree-manipulating.js
@@ -0,0 +1,13 @@
+const prettify = (root) => {
+  const divs = [...document.getElementsByTagName('div')];
+  divs.forEach((div) => {
+    const textNodes = [...div.childNodes]
+      .filter((child) => child instanceof Text)
+      .filter((child) => child.textContent.trim() !== '');
+    textNodes.forEach((node) => {
+      const p = document.createElement('p');
+      p.textContent = node.textContent;
+      node.replaceWith(p);
+    });
+  });
+};
