49bd9f7d8b9b84eb09645455908b73bd58f9c345
kelevra-pro <damtat8@gmail.com>
2022-05-21 16:43:35 +0300

    resolve js-dom progress

---
+++ 20-js-dom/progress.js
@@ -0,0 +1,10 @@
+const application = () => {
+  const element = document.querySelector('progress');
+  const handler = (counter = 0) => {
+    if (counter <= 100) {
+      element.setAttribute('value', counter);
+      setTimeout(() => handler(counter + 1), 1000);
+    }
+  };
+  handler();
+};
