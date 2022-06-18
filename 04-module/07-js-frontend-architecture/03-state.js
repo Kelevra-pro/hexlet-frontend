c7803d5438eab59d1f1c8e53adcbbe18c26b61b6
kelevra-pro <damtat8@gmail.com>
2022-06-18 20:19:06 +0300

    reslove js-frontend-architecture state

---
+++ 04-module/07-js-frontend-architecture/03-state.js
@@ -0,0 +1,30 @@
+const render = (state, formEl, inputEl, resultEl) => {
+  formEl.reset();
+  inputEl.focus();
+  resultEl.textContent = state;
+};
+
+const app = () => {
+  let state = 0;
+
+  const formEl = document.querySelector('form');
+  const inputEl = document.querySelector('input');
+  const resetEl = document.querySelector('button');
+  const resultEl = document.querySelector('#result');
+
+  formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    state += parseInt(data.get('number'), 10);
+    render(state, formEl, inputEl, resultEl);
+  });
+
+  resetEl.addEventListener('click', () => {
+    state = 0;
+    render(state, formEl, inputEl, resultEl);
+  });
+
+  inputEl.focus();
+};
+
+export default app;
